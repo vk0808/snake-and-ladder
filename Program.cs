ce7b32c2fe3c4aa946647792ffbd583c8b51e424
vk0808 <vk4765712@gmail.com>
2021-08-04 13:40:55 +0530

    feat: game run & restart

--- Program.cs
+++ Program.cs
@@ -18,28 +18,35 @@ namespace SnakeAndLadder
             Console.WriteLine("Game starts\n");
             Console.WriteLine("Player Position: " + player1Pos);
 
-            int dieRoll = rand.Next(1, 7);
-            int choice = rand.Next(0, 3);
-
-            Console.WriteLine("No on Die: " + dieRoll);
-            Console.WriteLine("Choice: " + choice);
-
-            switch (choice)
+            while (player1Pos < 100)
             {
-                case 1:
-                    Console.WriteLine("Ladder");
-                    player1Pos += dieRoll;
-                    Console.WriteLine("Player Position: " + player1Pos);
-                    break;
-                case 2:
-                    Console.WriteLine("Snake");
-                    player1Pos -= dieRoll;
-                    Console.WriteLine("Player Position: " + player1Pos);
-                    break;
-                default:
-                    Console.WriteLine("No play");
-                    Console.WriteLine("Player Position: " + player1Pos);
-                    break;
+
+                int dieRoll = rand.Next(1, 7);
+                int choice = rand.Next(0, 3);
+
+                Console.WriteLine("No on Die: " + dieRoll);
+                Console.WriteLine("Choice: " + choice);
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Ladder");
+                        player1Pos += dieRoll;
+                        break;
+                    case 2:
+                        Console.WriteLine("Snake");
+                        player1Pos -= dieRoll;
+                        break;
+                    default:
+                        Console.WriteLine("No play");
+                        break;
+                }
+
+                if (player1Pos < 0 )
+                {
+                    player1Pos = 0;
+                }
+                Console.WriteLine("Player Position: " + player1Pos + "\n");
             }
         }
     }
