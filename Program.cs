f5f9c4aa3e8d703b9e2de6a25a4c980f747406a2
vk0808 <vk4765712@gmail.com>
2021-08-04 12:34:11 +0530

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Simulation Program\n");
+        }
+    }
+}
