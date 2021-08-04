0ea30511140a346c31325010bcb3311fa834c8e6
vk0808 <vk4765712@gmail.com>
2021-08-04 12:47:36 +0530

    feat: game initialize

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Simulation Program\n");
+
+            /// Variables
+            int START = 0;
+            int END = 100;
+
+            int player1Pos = START;
+
+            Console.WriteLine("Game starts");
+            Console.WriteLine("Player Position: " + player1Pos);
         }
     }
 }
