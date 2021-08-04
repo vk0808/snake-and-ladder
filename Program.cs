604b4728820fbeb46b398c6a0ba7791c369e1b04
vk0808 <vk4765712@gmail.com>
2021-08-04 14:03:23 +0530

    feat: check game winner

--- Program.cs
+++ Program.cs
@@ -15,18 +15,22 @@ namespace SnakeAndLadder
             int player1Pos = START;
             Random rand = new Random();
 
+            /// Game start message
             Console.WriteLine("Game starts\n");
             Console.WriteLine("Player Position: " + player1Pos);
 
-            while (player1Pos < 100)
+            /// Game run
+            while (player1Pos < END)
             {
-
+                /// Rolling die
                 int dieRoll = rand.Next(1, 7);
                 int choice = rand.Next(0, 3);
 
                 Console.WriteLine("No on Die: " + dieRoll);
                 Console.WriteLine("Choice: " + choice);
 
+
+                /// Checking choice and updating position
                 switch (choice)
                 {
                     case 1:
@@ -42,10 +46,25 @@ namespace SnakeAndLadder
                         break;
                 }
 
-                if (player1Pos < 0 )
+
+                /// Check if player has reached below 0, then restart from 0
+                if (player1Pos < START )
                 {
-                    player1Pos = 0;
+                    player1Pos = START;
                 }
+
+                /// Check if player has  reached 100, then declare the player as winner
+                /// and check if player has reached above 100, then don't update position
+                if (player1Pos == END)
+                {
+                    Console.WriteLine("Congrats Player");
+                }
+                else if (player1Pos > END)
+                {
+                    player1Pos -= dieRoll;
+                }
+
+                /// Display current position at each play
                 Console.WriteLine("Player Position: " + player1Pos + "\n");
             }
         }
